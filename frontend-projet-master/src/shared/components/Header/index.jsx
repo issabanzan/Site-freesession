bbfdb0fd934ee13079a4a54203a5d607526c1279
issa <issabanzan.konate@yahoo.fr>
2024-02-19 13:22:31 +0100

    sixth commit to make the freesession responsive 19/02/2024

--- frontend-projet-master/src/shared/components/Header/index.jsx
+++ frontend-projet-master/src/shared/components/Header/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import logo from '../../../assets/Blue_Blob_Health_Care_Logo_4_-removebg-preview.png';
-import { LogIn, Home, Phone, Menu } from 'react-feather'; // Assurez-vous d'importer l'icône Menu ou une icône similaire pour le bouton
-
+import { LogIn, Home, Phone, Menu, X } from 'react-feather'; // Assurez-vous d'importer Menu et X pour le bouton de menu
 
 const navigationLinks = [
     {
@@ -26,37 +25,28 @@ function Header() {
     return (
         <header>
             <nav className="px-3 py-1 h-20 flex flex-row justify-between items-center max-w-8xl mx-auto">
-                <a className="navbar-brand flex items-center mt-6">
-                    <img src={logo} alt="logo" className="h-auto w-38 sm:w-47 md:w-36 lg:w-43 object-contain mt-1" />
+                <a className="navbar-brand flex items-center">
+                    <img src={logo} alt="logo" className="h-auto w-38 sm:w-47 md:w-36 lg:w-43 object-contain" />
                 </a>
-                {/* Bouton du menu hamburger pour les petits écrans */}
-                <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="sm:hidden">
-                    <Menu />
+                
+                {/* Bouton du menu pour les petits écrans */}
+                <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-white sm:hidden">
+                    {isMenuOpen ? <X /> : <Menu />}
                 </button>
-                {/* Menu déroulant qui s'affiche uniquement sur les petits écrans */}
-                <div className={`${isMenuOpen ? "block" : "hidden"} sm:hidden`}>
-                    <ul className="navbar-nav text-white">
+                
+                {/* Liens de navigation */}
+                <div className={`${isMenuOpen ? 'block' : 'hidden'} sm:block`}>
+                    <ul className="flex flex-col sm:flex-row text-white gap-3 items-center">
                         {navigationLinks.map((link, index) => (
                             <li className="nav-item px-3 py-1 bg-slate-700 flex items-center rounded-lg" key={index}>
                                 <a className="nav-link flex gap-2 items-center" href={link.url}>
                                     {link.icon}
-                                    <span>{link.label}</span>
+                                    <span className="text-base sm:text-lg md:text-xl lg:text-2xl">{link.label}</span>
                                 </a>
                             </li>
                         ))}
                     </ul>
                 </div>
-                {/* Liens de navigation pour les grands écrans */}
-                <ul className="navbar-nav text-white hidden sm:flex sm:flex-row gap-3 items-center mt-5">
-                    {navigationLinks.map((link, index) => (
-                        <li className="nav-item px-3 py-1 bg-slate-700 flex items-center rounded-lg" key={index}>
-                            <a className="nav-link flex gap-2 items-center" href={link.url}>
-                                {link.icon}
-                                <span className="text-base sm:text-lg md:text-xl lg:text-2xl">{link.label}</span>
-                            </a>
-                        </li>
-                    ))}
-                </ul>
             </nav>
         </header>
     );
