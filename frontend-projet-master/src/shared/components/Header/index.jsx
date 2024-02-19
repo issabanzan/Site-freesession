593a7e46ff28a0688850c4bfb790753e56c3c001
issa <issabanzan.konate@yahoo.fr>
2024-02-19 14:22:49 +0100

    Home page is now responsive 19/02/2024

--- frontend-projet-master/src/shared/components/Header/index.jsx
+++ frontend-projet-master/src/shared/components/Header/index.jsx
@@ -26,31 +26,30 @@ function Header() {
         <header>
             <nav className="px-3 py-1 h-20 flex flex-row justify-between items-center max-w-8xl mx-auto">
                 <a className="navbar-brand flex items-center">
-                    <img src={logo} alt="logo" className="h-auto w-38 sm:w-47 md:w-36 lg:w-43 object-contain" />
+                <img src={logo} alt="logo" className="h-auto w-20 sm:w-47 md:w-36 lg:w-43 object-contain mt-6" />
                 </a>
                 
                 {/* Bouton du menu pour les petits écrans */}
                 <button
                     onClick={() => setIsMenuOpen(!isMenuOpen)}
-                    className="sm:hidden p-2 bg-blue-500 border-2 border-white"
+                    className="sm:hidden p-2 bg-[#225886] border-2 border-white"
                     >
                     {isMenuOpen ? <X /> : <Menu />}
                     </button>
 
-                
                 {/* Liens de navigation */}
-                <div className={`${isMenuOpen ? 'block' : 'hidden'} sm:block`}>
-                    <ul className="flex flex-col sm:flex-row text-white gap-3 items-center">
-                        {navigationLinks.map((link, index) => (
-                            <li className="nav-item px-3 py-1 bg-slate-700 flex items-center rounded-lg" key={index}>
-                                <a className="nav-link flex gap-2 items-center" href={link.url}>
-                                    {link.icon}
-                                    <span className="text-base sm:text-lg md:text-xl lg:text-2xl">{link.label}</span>
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+<div className={`${isMenuOpen ? 'block' : 'hidden'} sm:block`}>
+    <ul className="flex flex-col sm:flex-row text-white gap-2 sm:gap-4 items-center mt-5">
+        {navigationLinks.map((link, index) => (
+            <li key={index} className="nav-item">
+                <a href={link.url} className="nav-link flex items-center gap-2 px-4 py-2 bg-[#225886] rounded-md transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110 shadow-lg">
+                    {link.icon}
+                    <span className="text-sm sm:text-base">{link.label}</span>
+                </a>
+            </li>
+        ))}
+    </ul>
+</div>
             </nav>
         </header>
     );
