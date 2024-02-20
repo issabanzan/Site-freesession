8f6614a10aa25b425eb3866a247316d660bd74ee
issa <issabanzan.konate@yahoo.fr>
2024-02-20 12:14:29 +0100

    Contact page is now responsive 2O/02/2024

--- frontend-projet-master/src/app_components/Contact/index.jsx
+++ frontend-projet-master/src/app_components/Contact/index.jsx
@@ -1,13 +1,11 @@
 import Layout from "../Layout";
 import React, { useState } from 'react';
-import { Heart, Home, Phone, Users, Mail } from 'react-feather';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 import WorkingRemotelyImage from '../../assets/bog.png';
 import WorkingRemotelyImage2 from '../../assets/but.png';
 import WorkingRemotelyImage3 from '../../assets/borgue.png';
-import axios from 'axios';
-import { useEffect } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
   const [name, setName] = useState('');
@@ -18,10 +16,10 @@ const Contact = () => {
     event.preventDefault(); 
     
     const contactData = { name, email, message };
-    console.log('Submitting form', { name, email, message }); 
+    console.log('Submitting form', contactData); 
 
     try {
-      const response = await axios.post('https://api.freesession.net/api/contact', contactData);
+      const response = await axios.post('http://localhost:8080/api/contact', contactData);
       if (response.status === 200) {
         setName('');
         setEmail('');
@@ -36,24 +34,24 @@ const Contact = () => {
 
   return (
     <Layout>
-      <div className="max-w-9xl">
-        <div className="flex justify-between items-center bg-cyan-500 ">
+      <div className="max-w-9xl mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-center bg-cyan-500 p-5">
           <div>
-            <h1 className="text-4xl font-semibold mb-5 text-white ml-10">Contactez-nous !</h1>
-            <p className="text-lg text-white font-medium max-w-xl ml-10">
+            <h1 className="text-3xl md:text-4xl font-semibold mb-5 text-white">Contactez-nous !</h1>
+            <p className="text-md md:text-lg text-white font-medium max-w-xl">
               Free your future or that of a loved one with the ADIOS method.
               Contact us for more information about our method,
               the sessions' process, scheduling appointments, or any other details.
             </p>
           </div>
-          <img src={WorkingRemotelyImage} alt="Working Remotely" className="w-1/1 max-w-xs mr-10"/>
+          <img src={WorkingRemotelyImage} alt="Working Remotely" className="w-full max-w-xs mt-4 md:mt-0 md:max-w-md lg:max-w-lg"/>
         </div>
 
-        <div className="bg-slate-800 max-w-9xl mx-auto flex justify-start items-center">
-          <img src={WorkingRemotelyImage2} alt="Working Remotely" className="w-1/1 max-w-xs mb-4"/>
-          <form className="mt-5 w-1/2 ml-10" onSubmit={handleSubmit}>
+        <div className="bg-slate-800 p-5 flex flex-col md:flex-row justify-start items-center">
+          <img src={WorkingRemotelyImage2} alt="Working Remotely" className="w-full max-w-xs mb-4 md:mb-0 md:max-w-md lg:max-w-lg"/>
+          <form onSubmit={handleSubmit} className="mt-5 md:mt-0 w-full md:w-1/2 lg:w-2/5 mx-auto md:ml-10">
             <div className="mb-4">
-              <label htmlFor="name" className="block text-sm font-medium text-white-600">
+              <label htmlFor="name" className="block text-sm font-medium text-white">
                 Name
               </label>
               <input
@@ -62,7 +60,7 @@ const Contact = () => {
                 name="name"
                 value={name}
                 className="mt-1 p-3 w-full border rounded-md text-white placeholder-gray-500"
-                placeholder="Votre nom"
+                placeholder="lastname and firstname"
                 onChange={(e) => setName(e.target.value)}
               />
             </div>
@@ -76,7 +74,7 @@ const Contact = () => {
                 name="email"
                 value={email}
                 className="mt-1 p-3 w-full border rounded-md text-white placeholder-gray-500"
-                placeholder="Votre adresse e-mail"
+                placeholder="mail"
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
@@ -87,26 +85,24 @@ const Contact = () => {
               <textarea
                 id="message"
                 name="message"
-                rows="2"
+                rows="4"
                 value={message}
                 className="mt-1 p-3 w-full border rounded-md text-white placeholder-gray-500"
-                placeholder="Votre message"
+                placeholder="messages"
                 onChange={(e) => setMessage(e.target.value)}
               ></textarea>
             </div>
-            
             <div className="mb-4">
               <button
                 type="submit"
-                className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                className="px-4 py-2 bg-[#225886] text-white rounded-md hover:bg-blue-600"
               >
                 Send
               </button>
             </div>
           </form>
-          
         </div>
-        
+        <ToastContainer />
       </div>
     </Layout>
   );
