162d6dd35e4fdb56e3841fb9afc0590cbe114414
issa <issabanzan.konate@yahoo.fr>
2024-02-25 14:33:29 +0100

    edited again section for Consultations by practice and Consultations by subject

--- frontend-projet-master/src/app_components/PresentationV2/components/Fonctionnalite7/index.jsx
+++ frontend-projet-master/src/app_components/PresentationV2/components/Fonctionnalite7/index.jsx
@@ -37,7 +37,7 @@ const consultationsData = {
 
 const ConsultationsPage = () => {
   return (
-    <div style={{ marginRight: '22rem' }}className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div style={{ marginRight: '18rem' }}className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="mb-12 mt-12">
         <h2 className="font-semibold text-lg mb-3 text-black">Consultations by practice</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-black">
