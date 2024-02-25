415ea2db283fc90e884a01d0eeb3475f1ed912f1
issa <issabanzan.konate@yahoo.fr>
2024-02-25 15:13:49 +0100

    sixth modification my Footer

--- frontend-projet-master/src/shared/components/Footer/index.jsx
+++ frontend-projet-master/src/shared/components/Footer/index.jsx
@@ -6,24 +6,24 @@ const Footer = () => {
       <div style={{ marginRight: '15rem' }} className="max-w-7xl mx-auto px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
           <div>
-            <h5 className="text-lg font-semibold mb-2">Call Free session</h5>
+            <h5 className="text-lg font-semibold mb-2">Call freesession</h5>
             <ul>
-              <li className="mb-1">Call Institutadios at <br /> 01 82 88 75 89</li>
+              <li className="mb-1">Call freesession at <br /> 01 82 88 75 89</li>
               <li>Send a message to <br /> contact@freesession.com</li>
             </ul>
           </div>
           <div>
-            <h5 className="text-lg font-semibold mb-2">Discover Free session</h5>
+            <h5 className="text-lg font-semibold mb-2">Discover freesession</h5>
             <ul>
               <li className="mb-1"><a href="/who-we-are" className="hover:underline">Who are we?</a></li>
               <li className="mb-1"><a href="/our-partners" className="hover:underline">Our partners</a></li>
-              <li className="mb-1"><a href="/media" className="hover:underline">Free session in the media</a></li>
+              <li className="mb-1"><a href="/media" className="hover:underline">Freesesession in the media</a></li>
               <li className="mb-1"><a href="/practitioners" className="hover:underline">Our practitioners by city</a></li>
               <li className="mb-1"><a href="/videos" className="hover:underline">Videos</a></li>
             </ul>
           </div>
           <div>
-            <h5 className="text-lg font-semibold mb-2">Institutadios commits</h5>
+            <h5 className="text-lg font-semibold mb-2">Freesession commits</h5>
             <ul>
               <li className="mb-1"><a href="?" className="hover:underline">How do we collect our reviews?</a></li>
             </ul>
