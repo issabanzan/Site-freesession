588e86a33d348ff558cc2867d49739fcc8c2db97
issa <issabanzan.konate@yahoo.fr>
2024-03-03 23:46:38 +0100

    I edited something

--- frontend-projet-master/src/app_components/PresentationV2/components/Fonctionnalite/index.jsx
+++ frontend-projet-master/src/app_components/PresentationV2/components/Fonctionnalite/index.jsx
@@ -19,20 +19,20 @@ const Fonctionnalite = () => {
         
         <div className="flex overflow-x-auto space-x-1">
           
-        <a href="" className="min-w-max p-4 rounded-md text-center flex flex-col justify-center items-center flex-shrink-0">
+        <a href="/chiropraxie" className="min-w-max p-4 rounded-md text-center flex flex-col justify-center items-center flex-shrink-0">
     <img src="/src/assets/chiropraxie-i.jpg" alt="alcools" className="w-60 h-40 object-cover rounded-md" />
     <p className="font-bold mt-2 text-black leading-tight tracking-tighter inter text-2xl">Chiropraxie</p>
     <p className="mt-1">recommended by 81%<br /> of our users</p>
   </a>
           
-  <a href="" className="min-w-max p-4 rounded-md text-center flex flex-col justify-center items-center flex-shrink-0">
+  <a href="/hypnosis" className="min-w-max p-4 rounded-md text-center flex flex-col justify-center items-center flex-shrink-0">
             <img src="/src/assets/hypnose-i.jpg" alt="mego" className="w-60 h-40 object-cover rounded-md" />
             <p className="font-bold mt-2 leading-tight tracking-tighter inter text-2xl text-black">Hypnosis</p>
             <p className="mt-1">recommended by 81% <br /> of our users</p>
             </a>
           
-            <a href="" className="min-w-max p-4 rounded-md text-center flex flex-col justify-center items-center flex-shrink-0">
-            <img src="/src/assets/Meditation.jpg" alt="traditionelle" className="w-50 h-40 object-cover rounded-md" />
+            <a href="/meditation" className="min-w-max p-4 rounded-md text-center flex flex-col justify-center items-center flex-shrink-0">
+            <img src="/src/assets/Meditation.jpg" alt="Meditation" className="w-50 h-40 object-cover rounded-md" />
             <p className="font-bold mt-2 leading-tight tracking-tighter inter text-2xl text-black">Meditation</p>
             <p className="mt-1">recommended by 81% <br /> of our users</p>
             </a>
--- frontend-projet-master/src/app_components/PresentationV2/components/Fonctionnalite2/index.jsx
+++ frontend-projet-master/src/app_components/PresentationV2/components/Fonctionnalite2/index.jsx
@@ -20,21 +20,26 @@ All disorders
         <div className="flex overflow-x-auto space-x-1">
           
           <div className="min-w-max p-4 rounded-md text-center flex flex-col justify-center items-center flex-shrink-0">
-          
-            <img src="/src/assets/skin-i.jpg" alt="peau" className="w-51 h-40 object-cover rounded-md" />
-            <p className="font-bold mt-2 leading-tight tracking-tighter inter text-2xl text-black">skin problem</p>
+          <a href="/stopsmoking" className="min-w-max p-4 rounded-md text-center flex flex-col justify-center items-center flex-shrink-0">
+            <img src="/src/assets/ventre.jpg" alt="peau" className="w-51 h-40 object-cover rounded-md" />
+            </a>
+            <p className="font-bold mt-2 leading-tight tracking-tighter inter text-2xl text-black">Stop smoking</p>
            
           </div>
           
           <div className="min-w-max p-4 rounded-md text-center flex flex-col justify-center items-center flex-shrink-0">
+          <a href="/fatigueandsleep" className="min-w-max p-4 rounded-md text-center flex flex-col justify-center items-center flex-shrink-0">
             <img src="/src/assets/fatigue-i.jpg" alt="fatigue" className="w-50 h-40 object-cover rounded-md" />
+            </a>
             <p className="font-bold mt-2 leading-tight tracking-tighter inter text-2xl text-black">fatigue and sleep</p>
            
           </div>
           
           <div className="min-w-max p-4 rounded-md text-center flex flex-col justify-center items-center flex-shrink-0">
-            <img src="/src/assets/medically.PNG" alt="Énergétique Traditionnelle Chinoise" className="w-50 h-40 object-cover rounded-md" />
-            <p className="font-bold mt-2 leading-tight tracking-tighter inter text-2xl text-black">PMA</p>
+          <a href="/allergie" className="min-w-max p-4 rounded-md text-center flex flex-col justify-center items-center flex-shrink-0">
+            <img src="/src/assets/allergie.jpg" alt="Énergétique Traditionnelle Chinoise" className="w-50 h-40 object-cover rounded-md" />
+            </a>
+            <p className="font-bold mt-2 leading-tight tracking-tighter inter text-2xl text-black">Allergies, intolerance</p>
             
           </div>
           
