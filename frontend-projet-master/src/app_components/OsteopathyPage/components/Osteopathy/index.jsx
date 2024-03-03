5f33491de8391afade05f8c6590ad2c351385973
issa <issabanzan.konate@yahoo.fr>
2024-03-03 15:23:46 +0100

    edited again osteopathie page

--- frontend-projet-master/src/app_components/OsteopathyPage/components/Osteopathy/index.jsx
+++ frontend-projet-master/src/app_components/OsteopathyPage/components/Osteopathy/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Osteopathy = () => {
   return (
-    <div style={{ marginRight: '18rem' }}  className="max-w-7xl mx-auto p-5">
+    <div style={{ marginRight: '16rem' }}  className="max-w-7xl mx-auto p-5">
       <h1 className="text-3xl font-bold mb-4 text-black font-semibold font-serif">Osteopathy</h1>
       <p className="mb-4 font-semibold font-serif">
       Osteopathy is a manual practice that aims to restore movement to tissues, organs and joints. 
