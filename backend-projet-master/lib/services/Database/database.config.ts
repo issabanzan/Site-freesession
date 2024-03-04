e325ed8b35529a9e708bebb93e416cb40360993c
issa <issabanzan.konate@yahoo.fr>
2024-03-04 14:25:19 +0100

    edited Database

--- backend-projet-master/lib/services/Database/database.config.ts
+++ backend-projet-master/lib/services/Database/database.config.ts
@@ -1,13 +1,13 @@
-export interface DatabaseConfiguration { // export de l'interface DatabaseConfiguration qui contient les informations de connexion à la base de données
+export interface DatabaseConfiguration {
   user: string;
   password: string;
   server: string;
   database: string;
 }
 
-const db_config: DatabaseConfiguration = { // constante db_config de type DatabaseConfiguration qui contient les informations de connexion à la base de données
-  user: "",
-  password: "",
+const db_config: DatabaseConfiguration = { 
+  user: "root",
+  password: "Banzan9771",
   server: "projet_mysql",
   database: "clients",
 };
