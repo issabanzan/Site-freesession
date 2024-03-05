710f3077f3a50ad9f303b2bd4b4d99332d8dba0b
issa <issabanzan.konate@yahoo.fr>
2024-03-05 01:34:57 +0100

    edited again responsives of addiction pages

--- frontend-projet-master/src/app_components/AddictionPage/components/Addiction/index.jsx
+++ frontend-projet-master/src/app_components/AddictionPage/components/Addiction/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Addiction = () => {
   return (
-    <div className="mt-8 mx-auto max-w-7xl sm:pl-0 lg:mr-[200px]">
+    <div className="mt-8 mx-auto max-w-7xl sm:pl-0 lg:mr-[220px]">
       <h1 className="text-3xl font-bold mb-4 text-black font-semibold font-serif">Addiction</h1>
       <p className="mb-4 font-semibold font-serif">
       Addiction is defined as the dependence or enslavement of an individual to an object or activity. 
