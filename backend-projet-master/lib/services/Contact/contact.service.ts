479c3928a3a8b6032ea1a3bdfd5ea4e2a9bd9cfe
issa <issabanzan.konate@yahoo.fr>
2024-03-08 02:33:22 +0100

    modifications 2

--- backend-projet-master/lib/services/Contact/contact.service.ts
+++ backend-projet-master/lib/services/Contact/contact.service.ts
@@ -1,44 +1,40 @@
-// fichier contact.service.ts qui contient le service pour l'envoi de mail
-
-import { ContactForm } from './contact.interface'; // import l'interface ContactForm depuis le fichier contact.interface.ts
-import * as nodemailer from 'nodemailer'; // import le module nodemailer pour envoyer des mails
-import { smtpConfig } from './contact.config'; // import la configuration de l'envoi de mail depuis le fichier contact.config.ts
+import { ContactForm } from './contact.interface';
+import * as nodemailer from 'nodemailer';
+import { smtpConfig } from './contact.config';
 
 export class ContactService {
-  private transporter: nodemailer.Transporter; // propriété transporter de type nodemailer.Transporter pour envoyer des mails
+  private transporter: nodemailer.Transporter;
 
   constructor() {
-    this.transporter = nodemailer.createTransport(smtpConfig); // un nouveau transporter avec la configuration smtpConfig
+    this.transporter = nodemailer.createTransport(smtpConfig);
   }
 
-  // méthode sendMail qui envoie un mail avec les informations du formulaire de contact
-  async sendMail(contactForm: ContactForm): Promise<void> { 
+  async sendMail(contactForm: ContactForm): Promise<void> {
     const mail = {
       from: contactForm.email,
-      to: 'contact@freesession.net.',
+      to: 'support@institutadios.com',
       subject: `Freesession request - ${contactForm.name}`,
       text: `Name: ${contactForm.name}\nEmail: ${contactForm.email}\nMessage: ${contactForm.message}`,
     };
     
-    const mailSender = { // mail de confirmation de réception
-      from: 'contact@freesession.net',
+    const mailSender = {
+      from: 'support@institutadios.com',
       to: contactForm.email,
       subject: 'Confirmation of receipt - Book free sessions with practitioners',
       text: 'Hello ${contactForm.name},\n\nWe have received your message and thank you for contacting us. A member of our team will process your request and respond to you as soon as possible.\n\nSincerely,\nThe Book free sessions with practitioners',
   };
 
  
-  await this.transporter.sendMail(mail); 
+  await this.transporter.sendMail(mail);
 
   
-  await this.transporter.sendMail(mailSender); 
+  await this.transporter.sendMail(mailSender);
 }
 
-// méthode sendResetEmail qui envoie un mail pour réinitialiser le mot de passe de l'utilisateur avec un lien de réinitialisation
 async sendResetEmail(email: string, token: string): Promise<void> {
-  const resetLink = `http://localhost:3000/reset-password?token=${token}`; 
-  const mail = { 
-    from: 'contact@freesession.net',
+  const resetLink = `http://localhost:3000/reset-password?token=${token}`;
+  const mail = {
+    from: 'support@institutadios.com',
     to: email,
     subject: 'Resetting your password',
     html: '<p>To reset your password, please click on this link: <a href="${resetLink}">${resetLink}</a></p>',
