5d549c9e4308ee5a1cded8c21d00fd06fa623fd1
Jiawei Xu <assistance@institutadios.com>
2024-03-08 23:28:38 +0000

    database.config.ts édité en ligne via Bitbucket

--- backend-projet-master/lib/services/Database/database.config.ts
+++ backend-projet-master/lib/services/Database/database.config.ts
@@ -1,15 +1,16 @@
-export interface DatabaseConfiguration {
+
+export interface DatabaseConfiguration { 
   user: string;
   password: string;
   server: string;
   database: string;
 }
 
-const db_config: DatabaseConfiguration = {
-  user: "root",
-  password: "Banzan9771",
-  server: "localhost",
-  database: "clients",
+const db_config: DatabaseConfiguration = {  
+  user: process.env.USERDATABASE, 
+  password: process.env.PASSWORDDATABASE,
+  server: process.env.SERVERDATABASE,
+  database: process.env.DATABASENAME,
 };
 
 export default db_config;
