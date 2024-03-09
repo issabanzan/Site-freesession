a3471efe72dab45c1a945b7a51d8cd3a3278ffd9
issa <issabanzan.konate@yahoo.fr>
2024-03-09 20:51:51 +0100

    edited header 3

--- frontend-projet-master/src/shared/components/Header/index.jsx
+++ frontend-projet-master/src/shared/components/Header/index.jsx
@@ -1,15 +1,13 @@
-import React, { useState } from 'react'; // usetate sert à gérer l'état d'un composant fonctionnel
+import React, { useState } from 'react';
 import logo from '../../../assets/Blue_Blob_Health_Care_Logo_4_-removebg-preview.png';
-import { LogIn, Menu, X } from 'react-feather';// import des icones
+import { LogIn, Menu, X } from 'react-feather';
 
-function Header() { // création du composant Header
-
-// création d'un état pour le menu ouvert ou fermé useState(false) permet de définir la valeur initiale de l'état
+function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  return ( // le return permet de retourner le code JSX qui sera affiché dans le navigateur
+  return (
     <header className="px-3 py-1 h-20 flex flex-row justify-between items-center max-w-8xl mx-auto">
-    
+     
      <a className="flex items-center">
         <img src={logo} alt="logo" className="h-auto w-20 sm:w-47 md:w-36 lg:w-43 object-contain mt-7 hidden sm:block" />
       </a>
@@ -20,14 +18,14 @@ function Header() { // création du composant Header
         Freesession
       </h1>
 
-      {/* Responsive Titre pour les petits écrans */}
+      {/* Titre pour les petits écrans */}
       <h1 style={{ marginRight: '14rem' }} className="text-xl font-bold text-[#333C4E] mt-7 leading-tight tracking-tighter inter text-2xl lg:hidden max-w-8xl mx-auto">
         Freesession
       </h1>
 
       
       
-      {/* Bouton menu pour mettre mes liens dedans pour les petits écrans */}
+      {/* Bouton menu à droite pour les petits écrans */}
       <div className="flex items-center lg:hidden mt-7">
         <button
           onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -40,7 +38,8 @@ function Header() { // création du composant Header
       <nav className={`${isMenuOpen ? 'flex' : 'hidden'} fixed inset-0 bg-[#225886] z-20 flex-col items-center justify-center`}>
         <a href="/AllSpecialities" className="text-white px-3 py-2 rounded-md text-base font-medium">Specialties</a>
         <a href="/AllDisorders" className="text-white px-3 py-2 rounded-md text-base font-medium">Troubles</a>
-        <a href="#" className="text-[#333C4E] font-bold mt-7 leading-tight tracking-tighter inter text-1xl">Home</a>
+        <a href="/blogs" className="text-white px-3 py-2 rounded-md text-base font-medium">Blogs</a>
+        <a href="/entreprise" className="text-white px-3 py-2 rounded-md text-base font-medium">Company Offers</a>
         <a href="/Contact" className="text-white px-3 py-2 rounded-md text-base font-medium">Contact Us</a>
         <a href="/login" className="text-white px-3 py-2 rounded-md text-base font-medium flex items-center">
           <LogIn className="mr-2" />
@@ -51,8 +50,8 @@ function Header() { // création du composant Header
       <div style={{ marginRight: '25rem' }}   className="hidden lg:flex items-center space-x-8">
         <a href="/AllSpecialities" className="text-[#333C4E] font-bold mt-7 leading-tight tracking-tighter inter text-1xl">Specialies</a>
         <a href="/AllDisorders" className="text-[#333C4E] font-bold mt-7 leading-tight tracking-tighter inter text-1xl">Disorders</a>
-        <a href="#" className="text-[#333C4E] font-bold mt-7 leading-tight tracking-tighter inter text-1xl">Home</a>
-        
+        <a href="/blogs" className="text-[#333C4E] font-bold mt-7 leading-tight tracking-tighter inter text-1xl">Blogs</a>
+        <a href="/entreprise" className="text-[#333C4E] font-bold mt-7 leading-tight tracking-tighter inter text-1xl">company offers</a>
       </div>
 
       
