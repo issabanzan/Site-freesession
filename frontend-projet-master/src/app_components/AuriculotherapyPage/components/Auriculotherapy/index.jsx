40f75692a5b6f573ec7458e04ec75698a1927f85
IssaBanzanKonate97 <151056816+IssaBanzanKonate97@users.noreply.github.com>
2024-03-12 19:42:43 +0100

    auriculo page

--- frontend-projet-master/src/app_components/AuriculotherapyPage/components/Auriculotherapy/index.jsx
+++ frontend-projet-master/src/app_components/AuriculotherapyPage/components/Auriculotherapy/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Auriculotherapy = () => {
   return (
-    <div className="mt-8 mx-auto max-w-7xl sm:pl-0 lg:mr-[240px]">
+    <div style={{ marginRight: '21rem' }} className="mt-8 mx-auto max-w-7xl sm:pl-0 lg:mr-[240px]">
       <h1 className="text-3xl font-bold mb-4 text-black font-semibold font-serif">Auriculotherapy</h1>
       <p className="mb-4 font-semibold font-serif">
       Auriculotherapy stimulates energy points on different locations in the outer ear. This stimulation can be carried out
