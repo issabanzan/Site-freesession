d8014abb8b8b355bd0055996f9d9f2e4347c05c1
IssaBanzanKonate97 <151056816+IssaBanzanKonate97@users.noreply.github.com>
2024-04-13 22:59:10 +0200

    link

--- frontend-projet-master/src/app_components/Loading/Loading.jsx
+++ frontend-projet-master/src/app_components/Loading/Loading.jsx
@@ -5,7 +5,7 @@ function Loading({ message, showLoader = true }) {
         <div className="z-30 bg-gray-50 inter
             fixed inset-0 flex items-center justify-center min-h-screen min-w-screen">
             <div className="flex flex-col items-center justify-center w-full">
-                <img src={app_logo} alt={import.meta.env.VITE_APPLICATION_NAME} className="w-auto h-30 mb-12" />
+                <img src={app_logo} alt={import.meta.env.VITE_APPLICATION_NAME} className="w-auto h-10 mb-12" />
                 {/* <h3 className="text-gray-800 text-lg font-medium mb-3 grotesk">
                     {message ? message : import.meta.env.VITE_APPLICATION_NAME}
                 </h3> */}
