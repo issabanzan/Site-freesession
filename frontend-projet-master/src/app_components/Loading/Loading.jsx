8dfe2405ca3322d083247f5c2a1927274d3cae49
IssaBanzanKonate97 <151056816+IssaBanzanKonate97@users.noreply.github.com>
2024-04-13 23:06:17 +0200

    loading 1

--- frontend-projet-master/src/app_components/Loading/Loading.jsx
+++ frontend-projet-master/src/app_components/Loading/Loading.jsx
@@ -2,18 +2,21 @@ import app_logo from '../../assets/Blue_Blob_Health_Care_Logo_4_-removebg-previe
 
 function Loading({ message, showLoader = true }) {
     return (
-        <div className="z-30 bg-gray-50 inter
-            fixed inset-0 flex items-center justify-center min-h-screen min-w-screen">
+        <div className="z-30 bg-gray-50 fixed inset-0 flex items-center justify-center min-h-screen min-w-screen">
             <div className="flex flex-col items-center justify-center w-full">
-                <img src={app_logo} alt={import.meta.env.VITE_APPLICATION_NAME} className="w-auto h-15 mb-12" />
-                {/* <h3 className="text-gray-800 text-lg font-medium mb-3 grotesk">
-                    {message ? message : import.meta.env.VITE_APPLICATION_NAME}
-                </h3> */}
-                {
-                    showLoader && <div className="bg-[#225886] h-2 max-w-xs w-full rounded-full mb-8 relative overflow-hidden">
+                <img src={app_logo} alt={import.meta.env.VITE_APPLICATION_NAME} className="w-auto h-12 mb-12" />
+                {/* Conditionnellement afficher le message ou le nom de l'application */}
+                {message ? (
+                    <h3 className="text-gray-800 text-lg font-medium mb-3">{message}</h3>
+                ) : (
+                    <h3 className="text-gray-800 text-lg font-medium mb-3">{import.meta.env.VITE_APPLICATION_NAME}</h3>
+                )}
+                {/* Afficher la barre de chargement si showLoader est true */}
+                {showLoader && (
+                    <div className="bg-[#225886] h-1 max-w-40 w-full rounded-full mb-8 relative overflow-hidden">
                         <div className="an_load_bar bg-cyan-600"></div>
                     </div>
-                }
+                )}
             </div>
         </div>
     );
